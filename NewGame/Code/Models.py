d07e9448c0c949d3427534c9afa332c735af5d18
octavio117 <73810246+octavio117@users.noreply.github.com>
2021-03-14 10:56:24 -0500

    Update Models.py

--- NewGame/Code/Models.py
+++ NewGame/Code/Models.py
@@ -9,15 +9,15 @@ class model():
         self.picture = pygame.transform.scale(self.picture, scale)
     def Flip(self):
         self.picture = pygame.transform.flip(self.picture, True, False)
-    def input_move(self, keys_pressed):
-        if keys_pressed[pygame.K_SPACE]:
-            self.hitbox.y -= 25
-        if keys_pressed[pygame.K_LEFT]:
-            self.hitbox.x -= 10
+    def movement(self, keys_pressed):
+        if keys_pressed[pygame.K_LEFT] and self.hitbox.x - 10 > 0:
+            return 'Left'
         if keys_pressed[pygame.K_RIGHT]:
-            self.hitbox.x += 10
+            return 'Right'      
+        if keys_pressed[pygame.K_SPACE]:
+            return 'Jump'
         if keys_pressed[pygame.K_DOWN]:
-            self.hitbox.y += 10
+            return 'Down'
 class Block():
     def __init__(self, name, Picture, Hitbox):
         self.name = name
@@ -25,4 +25,3 @@ class Block():
         self.hitbox = Hitbox
     def Scale(self, scale):
         self.picture = pygame.transform.scale(self.picture, scale)
-    
